b8400bbff387893ff33e093468dbe61a47759197
aditya <adityapandeyadp@gmail.com>
2024-10-20 16:36:42 +0530

    good for now

--- src/components/Home/Logo/index.jsx
+++ src/components/Home/Logo/index.jsx
@@ -1,39 +1,20 @@
-import { useRef, useEffect } from 'react';
-import vid from '../../../assets/cube.mp4';
-import './index.scss';
+import './index.scss'
+import vid from '../../../assets/cube.mp4'
 
 const Logo = () => {
-  const canvasRef = useRef(null);
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    const video = videoRef.current;
-
-    video.play();
-
-    const render = () => {
-      context.drawImage(video, 0, 0, canvas.width, canvas.height);
-      requestAnimationFrame(render);
-    };
-
-    render();
-  }, []);
 
   return (
     <div className='logo-container'>
       <video
-        ref={videoRef}
-        src={vid}
+        src={vid}  
         loop
         autoPlay
         muted
-        style={{ display: 'none' }} // Hide the actual video element
+        preload="auto" 
+        // style={{ display: 'block' }}
       />
-      <canvas ref={canvasRef} width={600} height={650} /> {/* Set desired dimensions */}
     </div>
-  );
+  )
 }
 
-export default Logo;
+export default Logo
