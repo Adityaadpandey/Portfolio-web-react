196f94db66985b9b7a35253dd0a572a52e46a46e
Aditya <Adityapandeyadp@gmail.com>
2024-11-02 03:30:41 +0530

    good one

--- src/components/Home/Logo/index.jsx
+++ src/components/Home/Logo/index.jsx
@@ -1,39 +1,11 @@
-import { useRef, useEffect } from 'react';
-import vid from '../../../assets/cube.mp4';
-import './index.scss';
+import Spline from '@splinetool/react-spline';
 
-const Logo = () => {
-  const canvasRef = useRef(null);
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    const video = videoRef.current;
-
-    video.play();
-
-    const render = () => {
-      context.drawImage(video, 0, 0, canvas.width, canvas.height);
-      requestAnimationFrame(render);
-    };
-
-    render();
-  }, []);
-
+export default function App() {
   return (
-    <div className='logo-container'>
-      <video
-        ref={videoRef}
-        src={vid}
-        loop
-        autoPlay
-        muted
-        style={{ display: 'none' }} // Hide the actual video element
-      />
-      <canvas ref={canvasRef} width={600} height={650} /> {/* Set desired dimensions */}
+
+    <div className='logo-container' >
+
+      <Spline scene="https://prod.spline.design/hDMiE0SH6-8VJQM0/scene.splinecode" />
     </div>
   );
 }
-
-export default Logo;
